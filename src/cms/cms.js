39eb7636e47de2b58f04ab4d18d1a65c698d8eb9
EmilBuszylo <emil.interesy@gmail.com>
2019-01-26 14:34:02 +0100

    added producents to cms settings

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,9 @@
 import CMS from 'netlify-cms'
 
-import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import ArticlePreview from './preview-templates/ArticlePreview'
-import ContactPagePreview from './preview-templates/ContactPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('contact', ContactPagePreview)
+CMS.registerPreviewTemplate('services-page', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', ArticlePreview)
