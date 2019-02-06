5948ee7c5b9a0e6faba8923ed4f44f4edd92ace9
ebuszylo <Emil.Buszylo@grupawp.pl>
2019-02-06 08:43:53 +0100

    wip

---
+++ src/templates/producents-page-bug.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { graphql } from 'gatsby';
+import ProducentsPageTemplate from '../components/ProducentsPageTemplate';
+
+export default class ProducentsPage extends Component {
+  render () {
+    const { frontmatter, html } = this.props.data.markdownRemark
+
+    return (
+      <div>
+        <ProducentsPageTemplate
+          title={frontmatter.title}
+          meta_description={frontmatter.meta_description}
+          meta_title={frontmatter.meta_title}
+          producents={frontmatter.producents}
+          content={html}
+        />
+      </div>
+    )
+  }
+}
+
+export const ProducentsPageQuery = graphql`
+  query ProducentsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        meta_title
+        meta_description
+        producents {
+          cover
+          name
+          description
+          link
+        }
+      }
+    }
+  }
+`
--- src/templates/producents-page.js
+++ src/templates/producents-page.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import { graphql } from 'gatsby';
-import ProducentsPageTemplate from '../components/ProducentsPageTemplate';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import { HTMLContent } from "../components/Content";
+import AboutPageTemplate from "../components/AboutPageTemplate";
 
-export default class ProducentsPage extends Component {
-  render () {
-    const { frontmatter, html } = this.props.data.markdownRemark
+const ProducentsPage = ({ data }) => {
+  const { markdownRemark: post } = data;
 
-    return (
-      <div>
-        <ProducentsPageTemplate
-          title={frontmatter.title}
-          meta_description={frontmatter.meta_description}
-          meta_title={frontmatter.meta_title}
-          producents={frontmatter.producents}
-          content={html}
-        />
-      </div>
-    )
-  }
-}
+  return (
+    <div>
+      <Helmet>
+        <title>{post.frontmatter.meta_title}</title>
+        <meta name="description" content={post.frontmatter.meta_description} />
+      </Helmet>
+      <AboutPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content=""
+      />
+    </div>
+  );
+};
+
+ProducentsPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export default ProducentsPage;
 
 export const ProducentsPageQuery = graphql`
   query ProducentsPage($id: String!) {
@@ -37,4 +46,4 @@ export const ProducentsPageQuery = graphql`
       }
     }
   }
-`
+`;
